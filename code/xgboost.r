1e5c9cb7ff42bcb9306cc5c5af26ec7aa7a12cc3
Jared Lander <info@jaredlander.com>
2018-09-21 10:08:59 -0400

    Started boosting

--- code/xgboost.r
+++ code/xgboost.r
@@ -20,3 +20,62 @@ histFormula <- HistoricDistrict ~ FireService +
     LotFront + LotDepth + BldgFront + 
     BldgDepth + LotType + Landmark + BuiltFAR +
     Built + TotalValue - 1
+
+landX_train <- build.x(histFormula, data=land_train,
+                       contrasts=FALSE, sparse=TRUE)
+landY_train <- build.y(histFormula, data=land_train) %>% 
+    as.integer() - 1
+head(landY_train, n=100)
+
+landX_test <- build.x(histFormula, data=land_test,
+                       contrasts=FALSE, sparse=TRUE)
+landY_test <- build.y(histFormula, data=land_test) %>% 
+    as.integer() - 1
+
+landX_val <- build.x(histFormula, data=land_val,
+                       contrasts=FALSE, sparse=TRUE)
+landY_val <- build.y(histFormula, data=land_val) %>% 
+    as.integer() - 1
+
+xgTrain <- xgb.DMatrix(data=landX_train, label=landY_train)
+xgVal <- xgb.DMatrix(data=landX_val, label=landY_val)
+
+xg1 <- xgb.train(
+    data=xgTrain,
+    objective='binary:logistic',
+    nrounds=1
+)
+
+xgb.plot.multi.trees(xg1, feature_names=colnames(landX_train))
+
+xg2 <- xgb.train(
+    data=xgTrain,
+    objective='binary:logistic',
+    nrounds=1,
+    eval_metric='logloss',
+    watchlist=list(train=xgTrain)
+)
+
+xg3 <- xgb.train(
+    data=xgTrain,
+    objective='binary:logistic',
+    nrounds=100,
+    eval_metric='logloss',
+    watchlist=list(train=xgTrain)
+)
+
+xg4 <- xgb.train(
+    data=xgTrain,
+    objective='binary:logistic',
+    nrounds=300,
+    eval_metric='logloss',
+    watchlist=list(train=xgTrain)
+)
+
+xg5 <- xgb.train(
+    data=xgTrain,
+    objective='binary:logistic',
+    nrounds=300,
+    eval_metric='logloss',
+    watchlist=list(train=xgTrain, validate=xgVal)
+)
